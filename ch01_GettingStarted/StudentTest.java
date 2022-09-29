d0497e388cea1045c595ce4deea4eb12b583c4a7
NianLi <linian0701@163.com>
2022-09-30 01:31:53 +0800

    add ch01

---
+++ ch01_GettingStarted/StudentTest.java
@@ -0,0 +1,15 @@
+
+
+public class StudentTest extends junit.framework.TestCase {
+    public void testCreate() {
+        Student student = new Student("Jane Doe");
+        String studentName = student.getName();
+        assertEquals("Jane Doe", studentName);
+
+        Student secondStudent = new Student("Joe Blow");
+        String secondStudentName = secondStudent.getName();
+        assertEquals("Joe Blow", secondStudentName);
+
+        assertEquals("Jane Doe", student.getName());
+    }
+}
