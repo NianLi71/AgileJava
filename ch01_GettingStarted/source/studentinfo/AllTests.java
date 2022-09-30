9b8908e5d3dd6114ded18b7c90cb9cb485d34181
NianLi <linian0701@163.com>
2022-10-01 00:45:37 +0800

    almost done ch02

---
+++ ch01_GettingStarted/source/studentinfo/AllTests.java
@@ -0,0 +1,20 @@
+
+/*
+javac -cp :$HOME/Documents/Codes/Java-Tools/lib/junit-3.8.1.jar AllTests.java;\
+java -cp .:$HOME/Documents/Codes/Java-Tools/lib/junit-3.8.1.jar junit.awtui.TestRunner AllTests
+ */
+
+package studentinfo;
+
+import junit.framework.TestSuite;
+
+public class AllTests {
+    public static TestSuite suite() {
+        final TestSuite suite = new TestSuite();
+
+        suite.addTestSuite(StudentTest.class);
+        suite.addTestSuite(CourseSessionTest.class);
+
+        return suite;
+    }
+}
---
+++ ch01_GettingStarted/source/studentinfo/Student.java
@@ -0,0 +1,15 @@
+
+
+package studentinfo;
+
+public class Student {
+    private String name;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
