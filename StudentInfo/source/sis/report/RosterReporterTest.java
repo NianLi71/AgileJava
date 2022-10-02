28f6782cdac12702fe492aa9f6a6272c92329020
NianLi <linian0701@163.com>
2022-10-02 14:14:18 +0800

    static method

--- StudentInfo/source/sis/report/RosterReporterTest.java
+++ StudentInfo/source/sis/report/RosterReporterTest.java
@@ -9,7 +9,7 @@ import sis.studentinfo.CourseSession;
 public class RosterReporterTest extends TestCase {
     public void testRosterReport() {
         CourseSession session = new CourseSession("ENGL", "101",
-                new DateUtil().createDate(2003, 1, 6));
+                DateUtil.createDate(2003, 1, 6));
 
         session.enroll(new Student("A"));
         session.enroll(new Student("B"));
--- StudentInfo/source/sis/studentinfo/DateUtil.java
+++ StudentInfo/source/sis/studentinfo/DateUtil.java
@@ -5,7 +5,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class DateUtil {
-    public Date createDate(int year, int month, int date) {
+    public static Date createDate(int year, int month, int date) {
         GregorianCalendar calendar = new GregorianCalendar();
         calendar.clear();
         calendar.set(Calendar.YEAR, year);
@@ -13,4 +13,6 @@ public class DateUtil {
         calendar.set(Calendar.DAY_OF_MONTH, date);
         return calendar.getTime();
     }
+
+    private DateUtil() {}
 }
